491b6991d499ead3a4882ef03f6c987a1e124408
Muhammed Furkan Gok <gkkangk@gmail.com>
2025-04-19 23:48:03 +0300

    Project Init

--- Assets/Scripts/GameState.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-[Serializable]
-public class GameState
-{
-    public float ballY;
-    public float ballVelocityY;
-    public bool isGrounded;
-}
--- Assets/Scripts/PlayerInput.cs
+++
@@ -1,5 +0,0 @@
-[System.Serializable]
-public struct PlayerInput
-{
-    public bool jumpPressed;
-}
